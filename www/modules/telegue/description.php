1f2d06a6477a85fde1698d8ce6378f07c3bdaf2c
Gol <hypergol@gmail.com>
2014-03-06 21:07:53 +0400

    Added Telegue module

---
+++ www/modules/telegue/description.php
@@ -0,0 +1,14 @@
+<?php
+
+/*****************************************************
+* modules/telegue/description.php
+* module description file
+* (c)virt2real.ru 2014
+* draft, by Gol
+/*****************************************************/
+
+$module_params['name'] = 'telegue';
+$module_params['title'] = 'Telegue';
+$module_params['menu_part'] = 10;
+$module_params['position'] = 2;
+?>
