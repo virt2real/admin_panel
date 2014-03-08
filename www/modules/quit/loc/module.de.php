5169ba4549a887d9772ebd84373812b9865449d9
The Chaos <keios.is.awake@night.org>
2014-03-08 16:27:50 +0100

    translation prototype: forgot to add module translations

---
+++ www/modules/quit/loc/module.de.php
@@ -0,0 +1,20 @@
+<?php
+
+/*****************************************************
+* modules/quit/loc/module.de.php
+* module german locale file
+*(c)virt2real.de 2014
+* draft, by keios
+/*****************************************************/
+
+$lang = array(// common items
+              'M_DESC'               => 'Ausloggen',
+              // module-specific
+              'M_YES'                => 'Ja',
+              'M_NO'                 => 'Nein, doch nicht',
+              'M_LEAVE_INTERFACE'    => 'Admininterface verlassen',
+              'M_REALLY_LEAVE'       => 'Ihre Session wird gelöscht, Sie werden sich erneut einloggen müssen. Wollen Sie dies tatsächlich?',
+              'M_LEAVE'              => 'ausloggen',
+              );
+
+?>
---
+++ www/modules/quit/loc/module.ru.php
@@ -0,0 +1,20 @@
+<?php
+
+/*****************************************************
+* modules/quit/loc/module.ru.php
+* module russian locale file
+*(c)virt2real.de 2014
+* draft, by keios
+/*****************************************************/
+
+$lang = array(// common items
+              'M_DESC'               => 'Выход',
+              // module-specific
+              'M_YES'                => 'Да',
+              'M_NO'                 => 'Я передумал',
+              'M_LEAVE_INTERFACE'    => 'Выйти из админки',
+              'M_REALLY_LEAVE'       => 'Ваша сессия будет стёрта, потребуется новая авторизация пользователя. Вы уверены что хотите этого?',
+              'M_LEAVE'              => 'выйти',
+              );
+
+?>
