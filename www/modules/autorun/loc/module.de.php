55e46a165c5920bd5c68a371f222e2c0264745e8
The Chaos <keios@gaussrifle.de>
2014-03-10 00:10:34 +0100

    translation prototype: fixes

--- www/modules/autorun/loc/module.de.php
+++ www/modules/autorun/loc/module.de.php
@@ -17,6 +17,7 @@ $lang = array(// common items
               'M_RIGHT_COLUMN'       => 'In der rechten Spalte sind die inaktiven Skripte (Verzeichnis /etc/init.d.sample), sie werden NICHT ausgeführt, sondern nur gespeichert.',
               'M_MOVE_ITEMS'         => 'Durch Verschieben der Skripte zwischen den Listen kann der Start aller nötigen Komponenten konfiguriert werden.',
               'M_V2R_AUTOSTART'      => 'virt2real Autostart-Skript',
+              'M_LINE_WRAPPING'      => 'Zeilenumbruch',
               );
 
 ?>
